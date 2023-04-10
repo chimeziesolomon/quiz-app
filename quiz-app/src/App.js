f7b325cbae1378918aa2a0e9319bb0745a879621
chimeziesolomon <chimezie.solomon.cs@gmail.com>
2023-04-10 22:18:08 +0100

    new commit

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,66 +1,43 @@
-import React, { Component } from 'react';
-import { useGlobalContext } from './context';
-
-import SetupForm from './SetupForm';
-import Loading from './Loading';
-import Modal from './Modal';
+import React, { useState, useContext, useEffect } from "react";
+import Quiz from "./Components/Quiz";
+import "./App.css";
+import DashBoard from "./Components/DashBoard";
+import EndScreen from "./Components/EndScreen";
+import { QuizContext } from "./Helpers/Context";
+import QuizChange from "./Components/QuizChange";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 function App() {
-  const {
-    waiting, 
-    loading,
-    questions,
-    index,
-    correct,
-    nextQuestion,
-    checkAnswer,
-  } = useGlobalContext()
-  if (waiting) {
-    return <SetupForm />
-  }
-  if (loading) {
-    return <Loading />
-  }
+    const [quizState, setQuizState] = useState("menu");
+    const [score, setScore] = useState(0);
+    const { quizzes, setQuizzes } = useContext(QuizContext);
 
-  const { question, incorrect_answers, correct_answer } = questions[index]
-  // const answers = [...incorrect_answers, correct_answers]
-  let answers = [...incorrect_answers]
-  const tempIndex = Math.floor(Math.random() * 4)
-  if (tempIndex === 3) {
-    answers.push(correct_answer)
-  } else {
-    answers.push(answers[tempIndex])
-    answers[tempIndex] = correct_answer
-  }
-    return (
-      <main>
-        <Modal />
-        <section className='quiz'>
-          <p className='correct-answers'>
-            correct answers : {correct}/{index}
-          </p>
-          <article className='container'>
-            <h2 dangerouslySetInnerHTML={{__html: question}} />
-            <div className='btn-container'>
-              {answers.map((answer, index) => {
-                return (
-                  <button 
-                  key={index}
-                  className='answer-btn'
-                  onClick={() => checkAnswer(correct_answer === answer)}
-                  />
-                  
-                )
-              })}
-            </div>
-          </article>
-          <button className='next-question' onClick={nextQuestion}>
-            next question
-          </button>
-        </section>
-      </main>
+
+    useEffect(() => {
+        const db = firebase.firestore();
+        const quizzesRef = db.collection("quizzes");
+
+        quizzesRef.onSnapshot((snapshot) => {
+            const quizzesData = [];
+            snapshot.forEach((doc) => {
+                const quiz = doc.data();
+                quizzesData.push(quiz);
+            });
+            setQuizzes(quizzesData);
+        });
+    }, []);
+
+    return ( <
+        div className = "App" >
+        <
+        h1 > Quiz App < /h1>{" "} <
+        QuizContext.Provider value = {
+            { quizState, setQuizState, score, setScore, quizzes }
+        } > { " " } { quizState === "menu" && < DashBoard / > } { " " } { quizState === "endScreen" && < EndScreen / > } { " " } { quizState === "quiz" && < Quiz / > } { " " } { quizState === "quizChange" && < QuizChange / > } { " " } <
+        /QuizContext.Provider>{" "} < /
+        div >
     );
-  }
-
+}
 
 export default App;
